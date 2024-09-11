17a547bbb60e90d874dcc6d104f9f385fb9175fe
CJavatX <cdpm98@hotmail.com>
2024-09-11 13:01:36 -0600

    checkAuthStatus al iniciar la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,22 +37,22 @@ export class AppComponent implements OnInit {
     return true;
   });
 
-  // public authStatusChangedEffect = effect(() => {
-  //   switch (this.authService.authStatus()) {
-  //     case AuthStatus.checking:
-  //       return;
+  public authStatusChangedEffect = effect(() => {
+    switch (this.authService.authStatus()) {
+      case AuthStatus.checking:
+        return;
 
-  //     case AuthStatus.authenticated:
-  //       console.log(this.destinationUrl);
-  //       console.log('Entró');
-  //       this.router.navigateByUrl(this.destinationUrl);
-  //       break;
+      case AuthStatus.authenticated:
+        console.log(this.destinationUrl);
+        console.log('Entró');
+        this.router.navigateByUrl(this.destinationUrl);
+        break;
 
-  //     case AuthStatus.notAuthenticated:
-  //       console.log(this.destinationUrl);
-  //       console.log('Entró');
-  //       this.router.navigateByUrl(this.destinationUrl);
-  //       break;
-  //   }
-  // });
+      case AuthStatus.notAuthenticated:
+        console.log(this.destinationUrl);
+        console.log('Entró');
+        this.router.navigateByUrl(this.destinationUrl);
+        break;
+    }
+  });
 }
